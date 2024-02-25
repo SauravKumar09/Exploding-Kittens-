f20fa29c0f87847a24ff801849216a0f53bdb783
Saurav Kumar <sauravkumar27894@gmail.com>
2024-02-25 14:01:51 +0530

    first commit

---
+++ exploding-kittens-game/client/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Form from "./Form";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 style={{ color: "#F24D11" }}>
+        EXPLODING <b style={{ color: "white" }}>KITTENS</b> 😸
+      </h1>
+      <Form />
+    </nav>
+  );
+}
+
+export default Navbar;
